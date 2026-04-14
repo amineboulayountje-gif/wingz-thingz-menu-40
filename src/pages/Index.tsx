d8a7a6994f7b1f738c420bc67ecaa2b9950300e9
amineboulayountje-gif <amine_boulayountje@hotmail.com>
2026-04-14 13:31:04 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import HeroSection from "@/components/HeroSection";
 import MenuSection from "@/components/MenuCard";
 import SidesAndDrinks from "@/components/SidesAndDrinks";
@@ -5,12 +6,23 @@ import OrderAndSocial from "@/components/OrderAndSocial";
 import { OrderProvider } from "@/context/OrderContext";
 
 const Index = () => {
+  const step2Ref = useRef<HTMLDivElement>(null);
+
+  const goToStep2 = () => {
+    step2Ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <OrderProvider>
       <div className="min-h-screen bg-background">
         <HeroSection />
-        <MenuSection />
-        <SidesAndDrinks />
+
+        <MenuSection onNext={goToStep2} />
+
+        <div ref={step2Ref}>
+          <SidesAndDrinks />
+        </div>
+
         <OrderAndSocial />
       </div>
     </OrderProvider>
